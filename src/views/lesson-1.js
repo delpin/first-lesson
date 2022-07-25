b6519a2704b72845193cbc6e1f02e6364cb5e305
KatiaProkina <prokina.katia@gmail.com>
2022-07-25 11:12:11 +0300

    1_lesson

--- src/views/lesson-1.js
+++ src/views/lesson-1.js
@@ -2,8 +2,36 @@ import './lesson-1.css';
 function Lesson1() {
     return (
         <div className="root">
-            {/* ... */}
+        <div>
+          <h2>Яндекс</h2>
+          <h1>Как сделать яндекс главным по поиску</h1>
+          <button>Установить расширение</button>
+          <h3>
+            Устанавливая программу, вы принимаете условия лицензионного соглашения
+          </h3>
         </div>
+        <div>
+          <div>
+            <div>
+              <img src="https://avatars.mds.yandex.net/get-lpc/1531919/6a1c901a-ee13-46af-9bb6-08d8e6360e0a/lqip"></img>
+            </div>
+            <h2>Установите расширение из Google Chrome Store</h2>
+          </div>
+          <div>
+            <div>
+              <img src="https://avatars.mds.yandex.net/get-lpc/1531919/f4b4d03e-934b-4b11-b9de-e2d4e649578f/lqip"></img>
+            </div>
+            <h2>Активируйте его, нажав на иконку у адресной строки</h2>
+          </div>
+          <div>
+            <div>
+              <img src="https://avatars.mds.yandex.net/get-lpc/1520633/3ed38931-24c1-49ed-931e-912985601262/lqip"></img>
+            </div>
+            <h2>Спрашивайте у Яндекса на стартовой и в адресной строке</h2>
+          </div>
+        </div>
+        <h4>© 2020 Яндекс</h4>
+      </div>
     );
 }
 
