a2af2cdcc2860e8f1ff88e2cd4a776f0f606cc3a
AmaliyaAli <amaliya_256@mail.ru>
2022-08-02 19:48:11 +0300

    Урок1

--- src/views/lesson-1.js
+++ src/views/lesson-1.js
@@ -1,9 +1,47 @@
 import './lesson-1.css';
 function Lesson1() {
     return (
-        <div className="root">
-            {/* ... */}
-        </div>
+        <div class="root">
+            <div>
+                <img alt="Яндекс" src="//yastatic.net/q/logoaas/v2/Яндекс.svg?size=25&amp;color=000"/>   
+            </div>
+            <div>
+                <h2>Как сделать Яндекс главным по поиску</h2>
+            </div>
+            <div>
+                <button>Установить расширение</button>
+            </div>
+            <div>
+                <span style="font-size: 9pt; line-height: 15px;">Устанавливая программу, вы принимаете условия Лицензионного соглашения</span>
+            </div>
+            <div>
+                <section data-row="0_default">
+                    <picture>
+                        <img src="https://avatars.mds.yandex.net/get-lpc/1531919/6a1c901a-ee13-46af-9bb6-08d8e6360e0a/fallback" width="580" height="400"></img>
+                    </picture>  
+                    <div>  
+                        <h4>Установите расширение из&nbsp;Google Chrome Store</h4>
+                    </div>  
+                </section>
+                <section data-row="0_default">   
+                    <picture>
+                        <img src="https://avatars.mds.yandex.net/get-lpc/1531919/f4b4d03e-934b-4b11-b9de-e2d4e649578f/fallback" width="580" height="400"></img>
+                    </picture>
+                    <div>
+                        <h4>Активируйте его, нажав на&nbsp;иконку у адресной строки</h4>
+                    </div> 
+                </section>
+                <section data-row="0_default">      
+                    <picture>
+                        <img src="https://avatars.mds.yandex.net/get-lpc/1520633/3ed38931-24c1-49ed-931e-912985601262/fallback" width="580" height="400"></img>
+                    </picture>   
+                    <div>
+                        <h4>Спрашивайте у Яндекса на&nbsp;стартовой и&nbsp;в&nbsp;адресной строке</h4> 
+                    </div>
+                </section>
+            </div>
+            <div>© 2020 Яндекс</div>
+            </div>
     );
 }
 
