0758d8c1c6b11835b2cff7c36da349587d4d9c7b
Рожин Алексей <a.rozhin@itfbgroup.ru>
2022-08-02 23:45:48 +0300

    2й урок

--- src/views/lesson-2.js
+++ src/views/lesson-2.js
@@ -1,11 +1,387 @@
-import './lesson-2.css';
+import "./lesson-2.css";
 
 function Lesson2() {
-    return (
-        <div className="root">
-            {/* ... */}
+  return (
+    <div className="roots">
+      <header class="header">
+        <div class="menu">
+          <div>
+            <img
+              src="https://sun9-9.userapi.com/impf/NV0ZJodUQM8v2g3i6QY62AuNkDNqBx-CpyDd0w/q7aBSEO9Ywc.jpg?size=46x47&quality=96&sign=7538bd32087c3ae418b8fc917f93f21d&type=album"
+              alt="YouTube"
+            ></img>
+          </div>
+          <div>
+            <img
+              src="https://sun9-20.userapi.com/impf/lLzqBaEVgwOGcQtbRv6f8BdHknyAOkqBXgalMA/YDHBZ8fb8X8.jpg?size=173x46&quality=96&sign=f1f688edbec9163ee0f83e3e7f0fb19f&type=album"
+              alt="YouTube"
+            ></img>
+          </div>
         </div>
-    );
+        <div class="content-search">
+          <div class="content-search content-search__input">
+            <input placeholder="Введите запрос" />
+          </div>
+          <div class="content-search content-search__button">
+            <img
+              src="https://sun9-51.userapi.com/impf/VJiahYGdzdoY5hbfW83erYLyN3wrEtHrrqW6ZA/Q1dvNwX1-qw.jpg?size=79x47&quality=96&sign=c0d081ebeab65527fd2a7ca5372afdda&type=album"
+              alt="Search"
+            />
+          </div>
+        </div>
+
+        <div class="content_2">
+          <div class="load_video">
+            <img
+              src="https://sun9-85.userapi.com/impf/JV9beNFly7SdJTcKhoX1CLaC9GyWX6V8W4GKlw/5rrBKvdxTVU.jpg?size=68x56&quality=96&sign=06bc15913aefe8427578c48a88e3509e"
+              alt="Load"
+            ></img>
+          </div>
+          <div class="notification">
+            <img
+              src="https://sun9-77.userapi.com/impf/Q-j2QweVL8mM7U9gsHehEgKInng5ToBSrJ6JIg/lksJU5rIT5o.jpg?size=57x56&quality=96&sign=7a8b04e7e7777ba46d4feec436c00a3e"
+              alt="Notific"
+            ></img>
+          </div>
+          <div class="login">
+            <img
+              src="https://sun9-24.userapi.com/impf/sEHeIp-5fesvUKbHuFODw3FaBsvl25UUkh3_Cw/LL_UZtZW1U0.jpg?size=67x56&quality=96&sign=15942dedb7afa1e839c78a2a48a7e577"
+              alt="Login"
+            ></img>
+          </div>
+        </div>
+      </header>
+
+      <div class="page-content">
+        <div class="NavigationBlock">
+          <div class="nav">
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M4,10V21h6V15h4v6h6V10L12,3Z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>Главная</label>
+              </div>
+            </div>
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M9.8,9.8l-3.83,8.23l8.23-3.83l3.83-8.23L9.8,9.8z M13.08,12.77c-0.21,0.29-0.51,0.48-0.86,0.54 c-0.07,0.01-0.15,0.02-0.22,0.02c-0.28,0-0.54-0.08-0.77-0.25c-0.29-0.21-0.48-0.51-0.54-0.86c-0.06-0.35,0.02-0.71,0.23-0.99 c0.21-0.29,0.51-0.48,0.86-0.54c0.35-0.06,0.7,0.02,0.99,0.23c0.29,0.21,0.48,0.51,0.54,0.86C13.37,12.13,13.29,12.48,13.08,12.77z M12,3c4.96,0,9,4.04,9,9s-4.04,9-9,9s-9-4.04-9-9S7.04,3,12,3 M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2 L12,2z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>Навигатор</label>
+              </div>
+            </div>
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>Shorts</label>
+              </div>
+            </div>
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M10,18v-6l5,3L10,18z M17,3H7v1h10V3z M20,6H4v1h16V6z M22,9H2v12h20V9z M3,10h18v10H3V10z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>Подписки</label>
+              </div>
+            </div>
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M10 9.35L15 12l-5 2.65zM12 7a5 5 0 105 5 5 5 0 00-5-5m0-1a6 6 0 11-6 6 6 6 0 016-6zm0-3a9 9 0 109 9 9 9 0 00-9-9m0-1A10 10 0 112 12 10 10 0 0112 2z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>YouTube Music</label>
+              </div>
+            </div>
+            <div class="nav-item">
+              <div class="icon-menu">
+                <svg viewBox="0 0 24 24">
+                  <path d="M11,7l6,3.5L11,14V7L11,7z M18,20H4V6H3v15h15V20z M21,18H6V3h15V18z M7,17h13V4H7V17z"></path>
+                </svg>
+              </div>
+              <div>
+                <label>Библиотека</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="content">
+          <div class="history-header">
+            <div class="history">
+              <div class="history-item">
+                <span>Все</span>
+              </div>
+              <div class="history-item">
+                <span>Сейчас в эфире</span>
+              </div>
+              <div class="history-item">
+                <span>Видеоигры</span>
+              </div>
+              <div class="history-item">
+                <span>Музыка</span>
+              </div>
+              <div class="history-item">
+                <span>Симуляторы</span>
+              </div>
+              <div class="history-item">
+                <span>Экшен</span>
+              </div>
+              <div class="history-item">
+                <span>Кулинария</span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="video" />
+              <div class="ytb-title">
+                <div class="icon"></div>
+                <div>
+                  <div class="video-title">
+                    <label>
+                      <b>Как быстро накопить на Квартиру?</b>
+                    </label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>Харчевников</label>
+                  </div>
+                  <div class="video-meta-block">
+                    <label>180 тыс. просмотров * 5 дней назад</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+   
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Lesson2;
